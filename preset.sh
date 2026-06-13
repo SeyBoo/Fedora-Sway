a54307026083335766d52c6555f3828594a8bcb6
seyboo <ad@antoinedv.com>
2026-06-13 08:13:34 +0200

    feat(installer): preset, auto-install, uninstall for Sway
    - preset.sh: replace xdph→wlr_portal, remove quickshell/ags keys,
      update default options to match Sway option key set
    - auto-install.sh: point Distro/URL to seyboo/Fedora-Sway, reword
      Hyprland→Sway throughout
    - uninstall.sh: swap package list to sway/swaybg/swayidle/swaylock/
      swww/wlsunset/xdg-desktop-portal-wlr/lxqt-policykit; remove
      hyprland/hypridle/hyprlock/hyprpaper/hyprsunset/hyprpolkitagent/
      quickshell entries; update COPR list and directory list accordingly

---
+++ preset.sh
@@ -0,0 +1,41 @@
+# https://github.com/JaKooLit / https://github.com/seyboo #
+
+# Define the options you want to preselect (either ON or OFF)
+# IMPORTANT: answer should be inside ""
+
+### Script will install nvidia-dkms nvidia-utils & nvidia-settings
+###-Would you like script to Configure NVIDIA for you?
+nvidia="OFF"
+
+###-Install GTK themes (required for Dark/Light function)?
+gtk_themes="ON"
+
+###-Do you want to configure Bluetooth?
+bluetooth="ON"
+
+###-Do you want to install Thunar file manager?
+thunar="ON"
+
+### Adding user to the 'input' group might be necessary for waybar keyboard-state functionality
+input_group="ON"
+
+###-Install & configure SDDM log-in Manager
+sddm="ON"
+### install and download SDDM themes
+sddm_theme="ON"
+
+###-Install XDG-DESKTOP-PORTAL-WLR? (For proper Screen Share ie OBS)
+wlr_portal="ON"
+
+### Shell extension. Bash alternative
+###-Install zsh, oh-my-zsh
+zsh="ON"
+### add Pokemon color scripts to terminal
+pokemon="ON"
+
+### This will install ASUSCTL & SUPERGFXCTL
+###-Installing on Asus ROG Laptops?
+rog="OFF"
+
+###-Download and Add pre-configured KooL-style Sway dotfiles?
+dots="ON"
